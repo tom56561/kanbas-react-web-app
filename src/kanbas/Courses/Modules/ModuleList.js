f4da14a19e68cf0bbba5c5249d12fc97232135b1
tom56561 <tom56561@gmail.com>
2023-10-29 15:33:45 -0400

    feat: add CRUD in Modules

--- src/kanbas/Courses/Modules/ModuleList.js
+++ src/kanbas/Courses/Modules/ModuleList.js
@@ -1,158 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import { FaGripVertical, FaSortDown, FaRegCheckCircle, FaPlus, FaEllipsisV } from "react-icons/fa";
 
 function ModuleList() {
     const { courseId } = useParams();
-    const modules = db.modules;
+    const [modules, setModules] = useState(db.modules);
+    const [module, setModule] = useState({
+        name: "New Module",
+        description: "New Description",
+        course: courseId,
+    });
+    const addModule = (module) => {
+        setModules([
+            { ...module, _id: new Date().getTime().toString() },
+            ...modules,
+        ]);
+    };
+    const deleteModule = (moduleId) => {
+        setModules(modules.filter(
+            (module) => module._id !== moduleId));
+    };
+    const updateModule = () => {
+        setModules(
+            modules.map((m) => {
+                if (m._id === module._id) {
+                    return module;
+                } else {
+                    return m;
+                }
+            })
+        );
+    }
+
     return (
         <div class="fs-4 mt-3">
-
             <ul className="list-group">
-                {
-                    modules
-                        .filter((module) => module.course === courseId)
-                        .map((module, index) => (
-                            <li key={index} className="py-4 list-group-item list-group-item-secondary mb-4">
-                                <FaGripVertical />
+                <li className="list-group-item">
+                    <input className="form-control" value={module.name}
+                        onChange={(e) => setModule({
+                            ...module, name: e.target.value
+                        })}
+                    />
+                    <textarea className="form-control" value={module.description}
+                        onChange={(e) => setModule({
+                            ...module, description: e.target.value
+                        })}
+                    />
+                    <button className="btn btn-primary mt-2 me-2" onClick={updateModule}> Update </button>
+                    <button className="btn btn-success mt-2" onClick={() => { addModule(module) }}>Add</button>
+                </li>
+
+                {modules
+                    .filter((module) => module.course === courseId)
+                    .map((module, index) => (
+                        <li key={index} className="py-4 list-group-item list-group-item-secondary mb-4">
+                            <FaGripVertical />
+                            <FaSortDown />
+                            {module.name}
+                            <span class="float-end">
+                                <FaRegCheckCircle className="text-success" />
                                 <FaSortDown />
-                                {module.name}
-                                <span class="float-end">
-                                    <FaRegCheckCircle className="text-success"/>
-                                    <FaSortDown />
-                                    <FaPlus />
-                                    <FaEllipsisV />
-                                </span>
-                                {/* <p>{module.description}</p> */}
-                            </li>
-                        ))
+                                <FaPlus />
+                                <FaEllipsisV />
+                                <button className="btn btn-warning me-1"
+                                    onClick={(event) => { setModule(module); }}>
+                                    Edit
+                                </button>
+                                <button className="btn btn-danger"
+                                    onClick={() => deleteModule(module._id)}>
+                                    Delete
+                                </button>
+                            </span>
+
+                            {/* <p>{module.description}</p> */}
+                        </li>
+                    ))
                 }
             </ul>
         </div>
     );
 }
 export default ModuleList;
-
-<div class="fs-4 mt-3">
-    <ul class="list-group w-100">
-        <li class="py-3 list-group-item list-group-item-secondary">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <i class="fa-solid fa-sort-down fa-xs"></i>
-            Week 0 - INTRO
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success"></i>
-                <i class="fa-solid fa-sort-down fa-sm"></i>
-                <i class="fa fa-plus px-3"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span>
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            LEARNING OBJECTIVES
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span>
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Introduction to the course
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Learn what is Web Development
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Creating a development environment
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Creating a Web Application
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Getting started with the 1st assignment
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> READING
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Full Stack - Chapter 1
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs pe-4"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> Full Stack - Chapter 2
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span> SLIDES
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <i class="fa-solid fa-link fa-2xs pe-4 text-success"></i>
-            <a href="#" class="text-danger text-decoration-none">
-                Introduction to Web Development
-                <i class="fa-solid fa-arrow-up-right-from-square fa-2xs"></i>
-            </a>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span>
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <i class="fa-solid fa-link fa-2xs pe-4 text-success"></i>
-            <a href="#" class="text-danger text-decoration-none">
-                Creating an HTTP server wiht Node.js
-                <i class="fa-solid fa-arrow-up-right-from-square fa-2xs"></i>
-            </a>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span>
-        </li>
-        <li class="py-3 list-group-item">
-            <i class="fa-solid fa-grip-vertical fa-2xs"></i>
-            <i class="fa-solid fa-link fa-2xs pe-4 text-success"></i>
-            <a href="#" class="text-danger text-decoration-none">
-                Creating a React Application
-                <i class="fa-solid fa-arrow-up-right-from-square fa-2xs"></i>
-            </a>
-            <span class="float-end">
-                <i class="fa fa-check-circle text-success pe-4"></i>
-                <i class="fa fa-ellipsis-v"></i>
-            </span>
-        </li>
-    </ul>
-</div>
