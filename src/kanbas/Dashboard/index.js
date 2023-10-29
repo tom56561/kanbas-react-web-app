baa96d0bd1deeda28d0abd79d3c052aba417ae9c
tom56561 <tom56561@gmail.com>
2023-10-29 14:44:49 -0400

    feat: add useState in Dashboard

--- src/kanbas/Dashboard/index.js
+++ src/kanbas/Dashboard/index.js
@@ -1,31 +1,93 @@
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import db from "../Database";
 function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+  });
+  const addNewCourse = () => {
+    setCourses([...courses,
+    {
+      ...course,
+      _id: new Date().getTime()
+    }]);
+  };
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div class="wd-dashboard-content">
       <h1>Dashboard</h1>
       <hr />
       <h2>Published Courses ({courses.length})</h2>
+      <h5>Course</h5>
+      <input value={course.name} className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+      <input value={course.number} className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })} />
+      <input value={course.startDate} className="form-control" type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
+      <input value={course.endDate} className="form-control" type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
+      <div className="mt-2">
+        <button onClick={addNewCourse} className="btn btn-success me-2" >
+          Add
+        </button>
+        <button onClick={updateCourse} className="btn btn-primary">
+          Update
+        </button>
+      </div>
+
       <div className="row">
         {courses.map((course) => (
           <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="col-xl-3 col-lg-4 col-md-6 py-3 text-decoration-none">
-              <div class="card" style={{width: '270px'}}>
-                <div class="card-img-top bg-primary" style={{height: '100px'}}>
-                  <a href="#" class="float-end p-4">
-                    <i class="fa-solid fa-ellipsis-vertical fa-2xl text-white"></i>
-                  </a>
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title text-primary">{course.name}</h5>
-                  <p class="card-text">CS5610.17387.202410_ Fall 2024 Semester Full Term...</p>
-                  <a href="#" class="course-card-icon">
-                    <i class="fa-regular fa-pen-to-square fa-lg text-secondary"></i> </a>
-                </div>
+            <div class="card" style={{ width: '270px' }}>
+              <div class="card-img-top bg-primary" style={{ height: '100px' }}>
+                <a href="#" class="float-end p-4">
+                  <i class="fa-solid fa-ellipsis-vertical fa-2xl text-white"></i>
+                </a>
               </div>
+              <div class="card-body">
+                <h5 class="card-title text-primary">{course.name}</h5>
+                <p class="card-text">CS5610.17387.202410_ Fall 2024 Semester Full Term...</p>
+                <a href="#" class="course-card-icon">
+                  <i class="fa-regular fa-pen-to-square fa-lg text-secondary"></i> </a>
+              </div>
+              <div className="m-2 d-flex justify-content-end">
+                <button
+                  onClick={(event) => {
+                    event.preventDefault();
+                    setCourse(course);
+                  }} className="btn btn-warning me-2">
+                  Edit
+                </button>
+                <button
+                  onClick={(event) => {
+                    event.preventDefault();
+                    deleteCourse(course._id);
+                  }} className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </div>
           </Link>
         ))}
       </div>
+
     </div>
   );
 }
