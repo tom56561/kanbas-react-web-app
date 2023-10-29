01cb3cc47493c136b86e4dca469a1c854c5cbe1d
tom56561 <tom56561@gmail.com>
2023-10-29 15:00:38 -0400

    refactor: refactor courese funtionto global

--- src/kanbas/Dashboard/index.js
+++ src/kanbas/Dashboard/index.js
@@ -1,34 +1,10 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import db from "../Database";
-function Dashboard() {
-  const [courses, setCourses] = useState(db.courses);
-  const [course, setCourse] = useState({
-    name: "New Course", number: "New Number",
-    startDate: "2023-09-10", endDate: "2023-12-15",
-  });
-  const addNewCourse = () => {
-    setCourses([...courses,
-    {
-      ...course,
-      _id: new Date().getTime()
-    }]);
-  };
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
-  };
-  const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
-  };
-
+function Dashboard(
+  { courses, course, setCourse, addNewCourse,
+    deleteCourse, updateCourse }
+) {
   return (
     <div class="wd-dashboard-content">
       <h1>Dashboard</h1>
--- src/kanbas/index.js
+++ src/kanbas/index.js
@@ -2,9 +2,34 @@ import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
+import db from "./Database";
+import { useState } from "react";
 
 
 function Kanbas() {
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "New Course",      number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+  });
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  };
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div className="d-flex">
       <KanbasNavigation />
@@ -12,9 +37,17 @@ function Kanbas() {
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses" element={<Courses />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
+          <Route path="Dashboard" element={
+            <Dashboard
+              courses={courses}
+              course={course}
+              setCourse={setCourse}
+              addNewCourse={addNewCourse}
+              deleteCourse={deleteCourse}
+              updateCourse={updateCourse}/>
+          } />
+          <Route path="Courses" element={<Courses courses={courses}/>} />
+          <Route path="Courses/:courseId/*" element={ <Courses courses={courses} />} />
         </Routes>
 
       </div>
