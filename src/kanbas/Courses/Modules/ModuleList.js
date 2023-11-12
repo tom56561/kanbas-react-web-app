c9d1822f946e45daf8464dcbb1d0e75f420ca62b
tom56561 <tom56561@gmail.com>
2023-11-12 15:40:56 -0500

    feat: add kanbas 4.3

--- src/kanbas/Courses/Modules/ModuleList.js
+++ src/kanbas/Courses/Modules/ModuleList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FaGripVertical, FaSortDown, FaRegCheckCircle, FaPlus, FaEllipsisV } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,10 +7,32 @@ import {
     deleteModule,
     updateModule,
     setModule,
+    setModules,
 } from "./modulesReducer";
+import * as client from "./client";
 
 function ModuleList() {
     const { courseId } = useParams();
+    useEffect(() => {
+        client.findModulesForCourse(courseId)
+            .then((modules) =>
+                dispatch(setModules(modules))
+            );
+    }, [courseId]);
+    const handleAddModule = () => {
+        client.createModule(courseId, module).then((module) => {
+            dispatch(addModule(module));
+        });
+    };
+    const handleDeleteModule = (moduleId) => {
+        client.deleteModule(moduleId).then((status) => {
+            dispatch(deleteModule(moduleId));
+        });
+    };
+    const handleUpdateModule = async () => {
+        const status = await client.updateModule(module);
+        dispatch(updateModule(module));
+    };
     const modules = useSelector((state) => state.modulesReducer.modules);
     const module = useSelector((state) => state.modulesReducer.module);
     const dispatch = useDispatch();
@@ -28,8 +50,8 @@ function ModuleList() {
                             ...module, description: e.target.value
                         }))}
                     />
-                    <button className="btn btn-primary mt-2 me-2" onClick={() => dispatch(updateModule(module))}> Update </button>
-                    <button className="btn btn-success mt-2" onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+                    <button className="btn btn-primary mt-2 me-2" onClick={() => handleUpdateModule()}> Update </button>
+                    <button className="btn btn-success mt-2" onClick={() => handleAddModule()}>Add</button>
                 </li>
 
                 {modules
@@ -49,7 +71,7 @@ function ModuleList() {
                                     Edit
                                 </button>
                                 <button className="btn btn-danger"
-                                    onClick={() => dispatch(deleteModule(module._id))}>
+                                    onClick={() => handleDeleteModule(module._id)}>
                                     Delete
                                 </button>
                             </span>
--- src/kanbas/Courses/Modules/modulesReducer.js
+++ src/kanbas/Courses/Modules/modulesReducer.js
@@ -1,9 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import db from "../../Database";
 
 
 const initialState = {
-  modules: db.modules,
+  modules: [],
   module: { name: "New Module 123", description: "New Description" },
 };
 
@@ -13,10 +12,7 @@ const modulesSlice = createSlice({
   initialState,
   reducers: {
     addModule: (state, action) => {
-      state.modules = [
-        { ...action.payload, _id: new Date().getTime().toString() },
-          ...state.modules,
-      ];
+      state.modules = [action.payload, ...state.modules];
     },
     deleteModule: (state, action) => {
       state.modules = state.modules.filter(
@@ -35,10 +31,13 @@ const modulesSlice = createSlice({
     setModule: (state, action) => {
       state.module = action.payload;
     },
+    setModules: (state, action) => {
+      state.modules = action.payload;
+    },
   },
 });
 
 
 export const { addModule, deleteModule,
-  updateModule, setModule } = modulesSlice.actions;
+  updateModule, setModule, setModules } = modulesSlice.actions;
 export default modulesSlice.reducer;
